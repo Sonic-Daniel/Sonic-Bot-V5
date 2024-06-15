66a6f781d5059745539fb1ac63698bc8a7b268b0
Sonic-Daniel <danielthiam54@gmail.com>
2024-06-15 01:36:51 +0100

    Update admin.js

--- scripts/cmds/admin.js
+++ scripts/cmds/admin.js
@@ -40,7 +40,7 @@ module.exports = {
 			removed: "✅ | 𝐋'𝐚𝐝𝐦𝐢𝐧 𝐚𝐲𝐚𝐧𝐭 𝐞𝐭𝐞 𝐫𝐞𝐭𝐢𝐫𝐞 𝐞𝐬𝐭 :\n%2",
 			notAdmin: "⚠️ | %1 users don't have admin role:\n%2",
 			missingIdRemove: "⚠️ | Please enter ID or tag user to remove admin role",
-			listAdmin: "🍀| 𝐀𝐃𝐌𝐈𝐍(𝐒) 𝐃𝐔 𝐁𝐎𝐓:\n━━━━━━━━━━━━━━━━\n🍂%1🍂"
+			listAdmin: "🍀| 𝐀𝐃𝐌𝐈𝐍(𝐒) 𝐃𝐔 𝐁𝐎𝐓:\n━━━━━━━━━━━━━━━━\n%1"
 		}
 	},
 
