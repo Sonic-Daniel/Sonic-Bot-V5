a02fa4f0c7384a357290df4f4fb18d965c77e544
ʚɸɞ Üčhïwa Shïsûį ʚɸɞ <158636500+Shisui-Uchiwa-Bot@users.noreply.github.com>
2024-06-21 03:58:35 +0100

    Update join.js

--- scripts/cmds/join.js
+++ scripts/cmds/join.js
@@ -5,7 +5,7 @@ module.exports = {
     version: "1.0",
     author: "Samir B. Thakuri",
     countDown: 5,
-    role: 0,
+    role: 2,
     shortDescription: {
       en: "Add user to support group",
     },
